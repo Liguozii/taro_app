716c5c493869ea024f055c2f4c59eedf9a57ed80
李果子 <>
2021-01-08 17:38:50 +0800

    init

---
+++ src/app.config.ts
@@ -0,0 +1,20 @@
+/*
+ * @Author: your name
+ * @Date: 2021-01-08 16:58:10
+ * @LastEditTime: 2021-01-08 17:14:58
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /taro_app/src/app.config.ts
+ */
+export default {
+  pages: [
+    'pages/index/index',
+    'pages/home/index',
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'Taro',
+    navigationBarTextStyle: 'black'
+  }
+}
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import './index.scss'
+
+export default class Home extends Component {
+
+  componentWillMount() { }
+
+  componentDidMount() { }
+
+  componentWillUnmount() { }
+
+  componentDidShow() { }
+
+  componentDidHide() { }
+
+  render() {
+    return (
+      <View className='home'>
+        <Text>Hello home!</Text>
+      </View>
+    )
+  }
+}
